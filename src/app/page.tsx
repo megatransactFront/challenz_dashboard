70e499eca48f54741b67d3eef05953b3900f33f6
Troy <troy.megaride@gmail.com>
2024-11-15 15:37:23 +0800

    Removed Duplicate Sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import Sidebar from "@/components/Sidebar";
 export default function Home() {
   return (
     <div className="flex">
-      <Sidebar />
       <main className="flex-1">
         <Dashboard />
       </main>
