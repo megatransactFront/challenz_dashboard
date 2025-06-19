1df246a411c9060eb32d5b89c0c09be567903fbe
kevinsus <99kevin.wong99@gmail.com>
2025-06-19 21:09:46 +1000

    added total business and users sections

--- src/app/business-users/page.tsx
+++ src/app/business-users/page.tsx
@@ -1,7 +1,27 @@
 const BusinessUserPage = () => {
   return (
-    <div>
-      Hello
+    <div className="p-8 space-y-4">
+      <h1 className="text-2xl font-bold">Business Registration</h1>
+      <div className="flex justify-between bg-white font-bold rounded-xl p-5">
+        <div className="space-y-1">
+          <p className="text-sm">Total Business</p>
+          <p className="text-lg">12,455</p>
+        </div>
+        <div className="space-y-1">
+          <p className="text-sm">New Users (Today)</p>
+          <p className="text-lg">5,455</p>
+        </div>
+        <div className="space-y-1">
+          <p className="text-sm">New Users (Week)</p>
+          <p className="text-lg">7000</p>
+        </div>
+        <div className="space-y-1">
+          <p className="text-sm">New Users (Month)</p>
+          <p className="text-lg">3000</p>
+        </div>
+      </div>
+      
+            
     </div>
   )
 }
